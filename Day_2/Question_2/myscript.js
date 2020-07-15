c8bbe9b9204be8068320cce11a2379d6846e5c95
Megha Shetty <56255348+MeghaShetty1398@users.noreply.github.com>
2020-07-15 11:43:57 -0700

    Update myscript.js

--- Day_2/Question_2/myscript.js
+++ Day_2/Question_2/myscript.js
@@ -1,3 +1,12 @@
 //String Methods
 
 var mytxt="Welcome to Javascript."
+//1. split()->A string can be converted to an array with the split() method
+console.log(mytxt.split(' '));
+//2.charCodeAt()->returns the unicode of the character at a specified index in a string
+console.log(mytxt.charCodeAt(8));    
+//3. ECMAScript 5 (2009) allows property access [ ] on strings
+console.log(mytxt[8]);
+//4.trim()->removes whitespace from both sides of a string
+var mytxt = "       Hello World!        ";
+console.log(mytxt.trim());
