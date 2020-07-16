ce8fccb1c90184cbc1b1e7a8c032e1fbfacc5fb5
Megha Shetty <56255348+MeghaShetty1398@users.noreply.github.com>
2020-07-15 22:23:28 -0700

    Update myscript.js

--- Day_2/Question_2/myscript.js
+++ Day_2/Question_2/myscript.js
@@ -10,3 +10,15 @@ console.log(mytxt[8]);
 //4.trim()->removes whitespace from both sides of a string
 var mytxt = "       Hello World!        ";
 console.log(mytxt.trim());
+//5.The replace() method replaces a specified value with another value in a string
+var mytxt="Hello Friends";
+console.log(mytxt.replace("Hello","Welcome"));
+//6.slice() extracts a part of a string and returns the extracted part in a new string.The method takes 2 parameters: the start position, and the end position (end not included).
+var str = "Apple, Banana, Kiwi";
+console.log(str.slice(7, 13));
+//substr() is similar to slice(). The difference is that the second parameter specifies the length of the extracted part.
+var str = "Apple, Banana, Kiwi";
+console.log(str.substr(7, 6));
+//The search() method searches a string for a specified value and returns the position of the match.
+var str = "Please locate where 'locate' occurs!";
+console.log(str.search("locate"));
