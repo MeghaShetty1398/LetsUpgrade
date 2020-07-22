4b64b94bffbad6332f81d435082ed266944e41a1
Megha Shetty <56255348+MeghaShetty1398@users.noreply.github.com>
2020-07-22 10:53:40 -0700

    Day 5->Question 3

--- Day_5/Question_3/myscript.js
+++ Day_5/Question_3/myscript.js
@@ -1,1 +1,12 @@
-console.log("TODOS");
+console.log("COMPLETED TODOS");
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response=>response.json())
+.then(
+    data=>{
+    for(var i=0;i<data.length;i++){
+        if(data[i].completed==true){
+            console.log(data[i]);
+        }
+        }
+    }
+);
